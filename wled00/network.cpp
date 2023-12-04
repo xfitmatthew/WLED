17adf0442a3a772d8aac0ee9b390d9f0353cc774
srg74 <28492985+srg74@users.noreply.github.com>
2023-12-04 17:09:08 -0500

    updates

--- wled00/network.cpp
+++ wled00/network.cpp
@@ -123,6 +123,8 @@ const ethernet_settings ethernetBoards[] = {
     18,                   // eth_mdio,
     ETH_PHY_LAN8720,      // eth_type,
     ETH_CLOCK_GPIO17_OUT  // eth_clk_mode
+  
+
   }
 };
 #endif
